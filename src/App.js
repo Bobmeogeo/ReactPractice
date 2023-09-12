09f01bf3e881e9f4146cf7c47f9cc4a3c9fd1fc6
bobmeogeo <phgyn1234@gmail.com>
2023-09-12 19:13:33 +0900

    R011. SpreadOperator: 이해 필요

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-// 001. jsx에 html 적용하기, 002. jsx에 css 적용하기
-
 import React from "react";
-import './App.css'; //css 적용
-import PropsDatatype from './R018_PropsDatatype';
+import './App.css';
+import SpreadOperator from './R011_SpreadOperator';
 
-function App(){
-  return(
+function App() {
+  return (
     <div>
       <h1>Start React 200!</h1>
-      <p>001. HTML 적용하기</p>
-      <p>002. css 적용하기</p>
-      <PropsDatatype
-      String = "react"
-      Number = {200}
-      Boolean = {1==1}
-      Array = {[0, 1, 8]}
-      ObjectJson = {{react:"리액트", twohundred:"200"}}
-      Function = {console.log("FunctionProps: function!")} 
-      />
+      <p>CSS 적용하기</p>
+      <SpreadOperator />
     </div>
   );
 }
